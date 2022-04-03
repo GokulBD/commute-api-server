818eb596cf3fa6459ce979aa04bfb232cb54fff2
GokulBD <bdgokul05@gmail.com>
2022-04-03 16:37:39 +0530

    add comments

--- src/routes/discussions.js
+++ src/routes/discussions.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const discussionSchema = mongoose.Schema({
+  username: String,
   topic: String,
   description: String
 });
 const Discussion = mongoose.model("Discussion", discussionSchema);
+
+const commentSchema = mongoose.Schema({
+  discussionId: String,
+  message: String
+});
+const Comment = mongoose.model("Comment", commentSchema);
 
 router.get("/", async (req, res, next) => {
   try {
@@ -34,6 +41,27 @@ router.post("/", async (req, res, next) => {
     const newDiscussions = new Discussion(req.body);
     newDiscussions.save();
     res.status(200).json({ message: "added successfully" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/comment", async (req, res, next) => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    const comment = new Comment(req.body);
+    comment.save();
+    res.status(200).json({ message: "added successfully" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/comment/:id", async (req, res, next) => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    const results = await Comment.find({ discussionId: req.params.id });
+    res.status(200).json({ results });
   } catch (error) {
     next(error);
   }
